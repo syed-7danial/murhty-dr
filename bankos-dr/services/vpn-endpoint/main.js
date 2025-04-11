0c3bef619605aa9325d91b68f37398d9b9475fa2
syed-7danial <muhammad.danial@eurustechnologies.com>
2025-04-11 05:19:50 +0500

    Updated

--- bankos-dr/services/vpn-endpoint/main.js
+++ bankos-dr/services/vpn-endpoint/main.js
@@ -1,3 +1,106 @@
+// const { custom_logging }  = require ('../../helper/helper.js')
+// const fs = require('fs');
+// const path = require('path');
+// const { program } = require('commander');
+// const { promisify } = require('util');
+// const chalk = require('chalk');
+// const { awsEnvironment }  = require ('../../helper/enum.js')
+// const readFileAsync = promisify(fs.readFile);
+// async function readAndParseFile(file) {
+//     const data = await readFileAsync(file, { encoding: 'utf-8' });
+//     const dataToJson = JSON.parse(data);
+//     return dataToJson
+//   }
+// const {  modifyVpnConnectionRoute } = require('../../helper/aws/ec2.js')
+
+// const AWS = require('aws-sdk');
+// AWS.config.update({
+//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//   sessionToken: process.env.AWS_SESSION_TOKEN,
+// maxRetries: 5, // Maximum number of retries
+//     retryDelayOptions: { 
+//         base: 200 // Base delay in milliseconds
+//     }
+// })
+// const processVpnEndpoint = async (environmentConfig) => {
+//     custom_logging(chalk.green("Starting process on Endpoints"))
+//     for (let vpn_endpoint of environmentConfig.vpn_endpoints) {
+//         let ec2toAddIpsClient, ec2toRemoveIpsClient, endpointToAddIpsTo, endPointToRemoveIpsFrom;
+    
+//         if (environmentConfig.switching_to === awsEnvironment.ACTIVE_ENV) {
+//             ec2toAddIpsClient = new AWS.EC2 ({region: environmentConfig.active_region});
+//             ec2toRemoveIpsClient = new AWS.EC2 ({region: environmentConfig.failover_region});
+//             endpointToAddIpsTo = vpn_endpoint.active_vpn_endpoint_id;
+//             endPointToRemoveIpsFrom = vpn_endpoint.failover_vpn_endpoint_id;
+//         }
+//         else {
+//             ec2toAddIpsClient = new AWS.EC2 ({region: environmentConfig.failover_region})
+//             ec2toRemoveIpsClient = new AWS.EC2 ({region: environmentConfig.active_region})
+//             endpointToAddIpsTo = vpn_endpoint.failover_vpn_endpoint_id
+//             endPointToRemoveIpsFrom = vpn_endpoint.active_vpn_endpoint_id;
+//         }
+        
+//         try {
+//             await modifyVpnConnectionRoute(ec2toAddIpsClient, endpointToAddIpsTo, vpn_endpoint.ip_list, true)
+//         }
+//         catch (error) {
+//             custom_logging(chalk.red("Error: ") + error.message);
+//         }
+
+//         if (global.PROCESS_CURRENT_ENVIRONMENT) {
+//             try {
+//                 await modifyVpnConnectionRoute(ec2toRemoveIpsClient, endPointToRemoveIpsFrom, vpn_endpoint.ip_list, false)
+//             }
+//             catch (error) {
+//                 custom_logging(chalk.red("Error: ") + error.message);
+//             }
+//         }
+//     }
+// }
+
+// const mainFunction = async () => {
+//     program
+//     .version('0.0.1')
+//     .option('-dr --dryRun', "Dry run the process")
+//     .option('-pce --processCurrentEnvironment', "Whether to perform the process on current environment")
+
+//     .parse(process.argv);
+    
+//     const options = program.opts();
+//     const file = path.resolve(__dirname, '..', '..', 'configuration', process.env.CLIENT_NAME, 'vpn_endpoint', 'configuration.json');
+//     let envs = await readAndParseFile(file)
+//     envs['switching_to'] = process.env.SWITCHING_TO
+//     envs['CLIENT_NAME'] = process.env.CLIENT_NAME
+//     console.log(envs)
+//     if (options.dryRun) {
+//         global.DRY_RUN = true;
+//         custom_logging(chalk.yellow("DRY RUN is enabled"))
+//     }
+//     else
+//         custom_logging(chalk.red("DRY RUN is disabled"))
+
+//     if (options.processCurrentEnvironment) {
+//         global.PROCESS_CURRENT_ENVIRONMENT = true;
+//         custom_logging(chalk.red("Current environment will be processed"))
+//     }
+//     else
+//         custom_logging(chalk.yellow("Current environment will not be processed"))
+
+//     custom_logging(`Switing to ${chalk.green(envs.switching_to)} environment`)
+//     await processVpnEndpoint(envs)
+//     custom_logging(chalk.green("Process has been completed"));
+// };
+
+// mainFunction()
+//     .then(() => {
+//         custom_logging(chalk.green("Exiting ..."));
+//     })
+//     .catch((error) => {
+//         custom_logging(chalk.red("Error: ") + error.message);
+//     });
+
+
 const { custom_logging }  = require ('../../helper/helper.js')
 const fs = require('fs');
 const path = require('path');
@@ -10,7 +113,7 @@ async function readAndParseFile(file) {
     const data = await readFileAsync(file, { encoding: 'utf-8' });
     const dataToJson = JSON.parse(data);
     return dataToJson
-  }
+}
 const {  modifyVpnConnectionRoute } = require('../../helper/aws/ec2.js')
 
 const AWS = require('aws-sdk');
@@ -18,11 +121,12 @@ AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   sessionToken: process.env.AWS_SESSION_TOKEN,
-maxRetries: 5, // Maximum number of retries
-    retryDelayOptions: { 
-        base: 200 // Base delay in milliseconds
-    }
+  maxRetries: 5,
+  retryDelayOptions: { 
+      base: 200
+  }
 })
+
 const processVpnEndpoint = async (environmentConfig) => {
     custom_logging(chalk.green("Starting process on Endpoints"))
     for (let vpn_endpoint of environmentConfig.vpn_endpoints) {
@@ -64,15 +168,30 @@ const mainFunction = async () => {
     .version('0.0.1')
     .option('-dr --dryRun', "Dry run the process")
     .option('-pce --processCurrentEnvironment', "Whether to perform the process on current environment")
+    .option('-all --allClients', "Run for all clients")
+    .option('-pcf --processCommonFolder', "Run the common folder config too")
 
     .parse(process.argv);
     
     const options = program.opts();
-    const file = path.resolve(__dirname, '..', '..', 'configuration', process.env.CLIENT_NAME, 'vpn_endpoint', 'configuration.json');
-    let envs = await readAndParseFile(file)
-    envs['switching_to'] = process.env.SWITCHING_TO
-    envs['CLIENT_NAME'] = process.env.CLIENT_NAME
-    console.log(envs)
+    const baseConfigPath = path.resolve(__dirname, '..', '..', 'configuration');
+    let clientsToProcess = [];
+
+    if (options.allClients) {
+        const allDirs = fs.readdirSync(baseConfigPath, { withFileTypes: true });
+        allDirs.forEach(dirent => {
+            if (dirent.isDirectory() && dirent.name !== 'common') {
+                clientsToProcess.push(dirent.name);
+            }
+        });
+    } else {
+        clientsToProcess.push(process.env.CLIENT_NAME);
+    }
+
+    if (options.processCommonFolder) {
+        clientsToProcess.push('common');
+    }
+
     if (options.dryRun) {
         global.DRY_RUN = true;
         custom_logging(chalk.yellow("DRY RUN is enabled"))
@@ -87,9 +206,23 @@ const mainFunction = async () => {
     else
         custom_logging(chalk.yellow("Current environment will not be processed"))
 
-    custom_logging(`Switing to ${chalk.green(envs.switching_to)} environment`)
-    await processVpnEndpoint(envs)
-    custom_logging(chalk.green("Process has been completed"));
+    for (const client of clientsToProcess) {
+        try {
+            const file = path.resolve(baseConfigPath, client, 'vpn_endpoint', 'configuration.json');
+            if (!fs.existsSync(file)) {
+                custom_logging(chalk.yellow(`Configuration file not found for ${client}, skipping.`));
+                continue;
+            }
+            let envs = await readAndParseFile(file)
+            envs['switching_to'] = process.env.SWITCHING_TO
+            envs['CLIENT_NAME'] = client
+            custom_logging(`Switing to ${chalk.green(envs.switching_to)} environment for client ${chalk.cyan(client)}`)
+            await processVpnEndpoint(envs)
+            custom_logging(chalk.green(`Process completed for ${client}`));
+        } catch (error) {
+            custom_logging(chalk.red(`Error while processing ${client}: `) + error.message);
+        }
+    }
 };
 
 mainFunction()
